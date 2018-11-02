ab682511a1914729a1430fb70d59465fdb78cd9d
enigzhg <ning.n.zhang@ericsson.com>
2018-11-02 14:56:23 +0800

    Remove redundant code in InterceptorRecursive.java. Change pom version to 1.2.2-SNAPSHOT

--- src/main/java/com/ericsson/commonlibrary/proxy/InterceptorRecursive.java
+++ src/main/java/com/ericsson/commonlibrary/proxy/InterceptorRecursive.java
@@ -21,11 +21,6 @@ SOFTWARE. SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 package com.ericsson.commonlibrary.proxy;
 
-import java.lang.reflect.Modifier;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * An utility decoration {@link Interceptor} that makes itself and the real provided
  * {@link Interceptor} to be added to every returned object on the intercepted Object, t
@@ -34,8 +29,6 @@ import org.slf4j.LoggerFactory;
  * @author Elis Edlund (elis.edlund@ericsson.com)
  */
 final class InterceptorRecursive implements Interceptor {
-
-    private static final Logger LOG = LoggerFactory.getLogger(InterceptorRecursive.class);
 
     private Interceptor actualInterceptor;
     private boolean shouldInterceptPrivateMethods = false;
@@ -53,37 +46,6 @@ final class InterceptorRecursive implements Interceptor {
     public Object intercept(Invocation invocation) throws Throwable {
         Object returnObject = actualInterceptor.intercept(invocation);
         return Util.tryToAddInterceptorToObject(returnObject, this, invocation, shouldInterceptPrivateMethods);
-    }
-
-    static Object tryToAddInterceptorToObject(Object object, Interceptor interceptor,
-            Invocation invocation,
-            boolean shouldInterceptPrivateMethods) {
-
-        if (object == null) {
-            return null;
-        }
-        if (!Util.isClassSafeFromPublicVariableProblems(object.getClass())) {
-            LOG.debug(
-                    "Class: {} is not safe for recursive addition of interceptor {}. Will therefore NOT add the recursive interceptor",
-                    object.getClass().getSimpleName(),
-                    interceptor.getClass().getSimpleName());
-            return object;
-        }
-        if (!shouldInterceptPrivateMethods && Modifier.isPrivate(invocation.getMethod().getModifiers())) {
-            return object;
-        }
-        if (Util.doesObjectHaveInterceptor(object, interceptor)) {
-            return object;
-        }
-        try {
-            return Proxy.intercept(object, interceptor);
-        } catch (Throwable t) {
-            LOG.trace("Failed to add interceptor: {} to the return object of type: {} returned by method:",
-                    interceptor.getClass().getSimpleName(),
-                    invocation.getMethod().getReturnType().getSimpleName(),
-                    invocation.getMethodName());
-            return object;
-        }
     }
 
 }
